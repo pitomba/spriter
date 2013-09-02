c2b47473c04b828a3af18d2515747c8ecdc9f4b2
Klaus Laube <kplaube@gmail.com>
2013-09-02 19:17:12 -0300

    Fixed gen_image import in watcher.py

--- spriter/watcher.py
+++ spriter/watcher.py
@@ -1,4 +1,4 @@
-from gen_image import Sprite
+from sprite import Sprite
 from watchdog.observers import Observer
 import ConfigParser
 import os
@@ -12,18 +12,19 @@ config.readfp(open(config_file, "r"))
 img_dir = config.get("dirs", "img_dir")
 css_dir = config.get("dirs", "css_dir")
 sprite_dir = config.get("dirs", "sprite_dir")
-class_base = config.get("dirs", "class_base")
+class_name = config.get("dirs", "class_name")
 
 
 class Handler(object):
     def dispatch(self, event):
+        if event.is_directory:
+            return False
 
-        if not event.is_directory:
-            path = os.path.dirname(event.src_path)
-            files = [os.path.join(path, arq) for arq in os.listdir(path)]
-            sprite = Sprite(files, css_path=css_dir, sprite_path=sprite_dir,
-                            class_base=class_base)
-            sprite.gen_sprite()
+        path = os.path.dirname(event.src_path)
+        files = [os.path.join(path, arq) for arq in os.listdir(path)]
+        sprite = Sprite(files, css_path=css_dir, sprite_path=sprite_dir,
+                        class_name=class_name)
+        sprite.gen_sprite()
 
 if __name__ == "__main__":
     event_handler = Handler()

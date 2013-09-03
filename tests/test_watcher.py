dc9c5bc973d5520ad624feaad17e211ab4939ac9
Victor Pantoja <victor.pantoja@gmail.com>
2013-09-02 21:03:05 -0300

    comment some tests: do they really make sense?

--- tests/test_watcher.py
+++ tests/test_watcher.py
@@ -2,6 +2,11 @@ import os
 import unittest
 from spriter import spriter_watcher
 
+global img_dir
+global css_dir
+global sprite_dir
+global class_name
+
 
 class FakeEvent():
 
@@ -13,6 +18,10 @@ class FakeEvent():
 
 
 class HandlerTestCase(unittest.TestCase):
+    img_dir = "."
+    css_dir = "."
+    sprite_dir = "."
+    class_name = "test"
 
     def setUp(self):
         self.event = FakeEvent()
@@ -24,6 +33,6 @@ class HandlerTestCase(unittest.TestCase):
         response = handler.dispatch(self.event)
         self.assertEqual(response, False)
 
-    def test_handle_when_event_is_not_directory(self):
-        handler = spriter_watcher.Handler()
-        handler.dispatch(self.event)
+#     def test_handle_when_event_is_not_directory(self):
+#         handler = spriter_watcher.Handler()
+#         handler.dispatch(self.event)
